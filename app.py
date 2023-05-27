91f4e6d3e0341a20364bebaefe8e1eb379407914
Aditya Velusamy <adityavelusamy@gmail.com>
2023-05-27 14:58:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import gradio as gr
+from transformers import pipeline
+from huggingface_hub import hf_api
 
+
+pipe = pipeline("text-generation", model="adityavelusamy/quest-v3")
 def contentText(conText):
     return conText
-
 def combine(context, numQuestions):
-    return 'Context:\n\n' + context + '\n\nExample:\n\nQ --> The sun is responsible for\nC --> { "text": [ "puppies learning new tricks", "children growing up and getting old", "flowers wilting in a vase", "plants sprouting, blooming and wilting" ], "label": [ "A", "B", "C", "D" ] }\nA --> D\n\nGenerate ' + str(numQuestions) + ' more multiple choice questions of this format whose answers can be found directly in the context provided.'
+    prompt = 'Context:\n\n' + context + '\n\nExample:\n\nQ --> The sun is responsible for\nC --> { "text": [ "puppies learning new tricks", "children growing up and getting old", "flowers wilting in a vase", "plants sprouting, blooming and wilting" ], "label": [ "A", "B", "C", "D" ] }\nA --> D\n\nGenerate ' + str(numQuestions) + ' more multiple choice questions of this format whose answers can be found directly in the context provided.'
+    return pipe(prompt)
 
 with gr.Blocks() as demo:
     content = gr.Textbox(label="Content", lines=2)
-    nQuestions = gr.Slider(0, 25, label="Number of Questions", step=1)
+    nQuestions = gr.Slider(1, 25, label="Number of Questions", step=1)
     out = gr.Textbox(value="", label="Output")
     btn = gr.Button(value="Generate")
     btn.click(combine, inputs=[content, nQuestions], outputs=[out])
